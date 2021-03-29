939d01ff7e727545f4f28ba60230dc14ad195da1
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-03-29 18:35:04 +0530

    remove unused imports

--- julia/modules/Country_🚩.py
+++ julia/modules/Country_🚩.py
@@ -1,7 +1,19 @@
-from telethon.tl.functions.photos import GetUserPhotosRequest
-from telethon.tl.functions.users import GetFullUserRequest
-from telethon.tl.types import MessageEntityMentionName
-from telethon.utils import get_input_location
+#    MissJuliaRobot (A Telegram Bot Project)
+#    Copyright (C) 2019-2021 Julia (https://t.me/MissJulia_Robot)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, in version 3 of the License.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see < https://www.gnu.org/licenses/agpl-3.0.en.html/ >.
+
+
 import flag
 import html, os
 from countryinfo import CountryInfo
