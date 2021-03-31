4db686d749fc1b5e06436efbddd21eedf5d88a6e
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-03-31 23:55:47 +0530

    Update YouTube_🎬.py

--- julia/modules/YouTube_🎬.py
+++ julia/modules/YouTube_🎬.py
@@ -166,7 +166,7 @@ async def yts_search(video_q):
 **Channel Link**: `{chnl}`
 **Video Link**: `{vlink}`
 """
-    await video_q.send_file(event.chat_id, f, reply_to=event.sender_id, file_name="thumb.jpg", caption=final)
+    await tbot.send_file(event.chat_id, f, reply_to=event.sender_id, file_name="thumb.jpg", caption=final)
     
 
 file_help = os.path.basename(__file__)
